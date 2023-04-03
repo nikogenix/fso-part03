12ad0e6c5cffedcfb691676cf67f29a0790c98f8
justKara <nikogenix@gmail.com>
2023-04-03 23:36:32 +0300

    Updating part 3

--- index.js
+++ index.js
@@ -28,6 +28,13 @@ app.get("/api/persons", (req, res) => {
 	res.json(persons);
 });
 
+app.get("/info", (req, res) => {
+	const count = persons.length;
+	const time = new Date();
+	res.send(`<p>Phonebook has info for ${count} people<p/>
+    <i>${time}<i/>`);
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`);
