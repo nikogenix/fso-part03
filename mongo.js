353c678e239c22df3b66feb5709926396f6a376f
justKara <nikogenix@gmail.com>
2023-04-06 02:47:47 +0300

    Updating part 3

---
+++ mongo.js
@@ -0,0 +1,52 @@
+// To add a new entry, run:
+// node mongo.js NAME NUMBER
+// To check the current entries, run:
+// node mongo.js
+
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const db = `mongodb+srv://nikogenix:${process.env.MONGODB_PASSWORD}@cluster0.mnl0osy.mongodb.net/phonebookApp?retryWrites=true&w=majority`;
+
+if (process.argv.length === 2) {
+	mongoose.set("strictQuery", false);
+	mongoose.connect(db);
+
+	const personSchema = new mongoose.Schema({
+		name: String,
+		number: String,
+	});
+
+	const Person = mongoose.model("Person", personSchema);
+
+	Person.find({}).then((result) => {
+		result.forEach((person) => {
+			console.log(person);
+		});
+		mongoose.connection.close();
+	});
+} else if (process.argv.length === 4) {
+	mongoose.set("strictQuery", false);
+	mongoose.connect(db);
+
+	const personSchema = new mongoose.Schema({
+		name: String,
+		number: String,
+	});
+
+	const Person = mongoose.model("Person", personSchema);
+
+	const person = new Person({
+		name: process.argv[2],
+		number: process.argv[3],
+	});
+
+	person.save().then((result) => {
+		console.log(result);
+		console.log("person added!");
+		mongoose.connection.close();
+	});
+} else {
+	console.error("you can use 2 arguments to add a new entry, or no arguments to check the phonebook");
+	process.exit(1);
+}
