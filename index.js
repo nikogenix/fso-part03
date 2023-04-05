b094cc64a511dd4f88323dc18b7f478ab696b317
justKara <nikogenix@gmail.com>
2023-04-05 03:04:08 +0300

    Updating part 3

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+
+app.use(cors());
 
 app.use(express.json());
 
@@ -103,7 +106,7 @@ app.post("/api/persons", (req, res) => {
 	res.json(person);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`);
 });
